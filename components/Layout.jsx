4ec30e50b6b6907418371b68d9c8585b4da3f9d0
bill3129066 <bill3129066@gmail.com>
2023-12-12 14:26:51 +0300

    style: change logo

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
-// Layout created to keep banner from streching to eachside of the page
+
 const Layout = ({ children }) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://dev.biznius.ai/static/chat.js";
+    script.onload = () => {
+      initializeChatWidget({
+        token: "da1b5880-431c-4c5a-a",
+      });
+    };
+
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div className="layout">
       <Head>
-        <title>Recipe Roulette</title>
+        <title>BizDelicious</title>
       </Head>
       <header>
         <Navbar />
@@ -23,4 +35,4 @@ const Layout = ({ children }) => {
   )
 }
 
-export default Layout
+export default Layout;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,57 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai'
-
+import { AiOutlineShopping } from 'react-icons/ai';
 import { Cart } from '.';
-import { useStateContext} from '../context/StateContext';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
-  return (
-    <div className="navbar-container">
-      <p className="logo">
-        <Link href="/">Recipe Roulette</Link>
-      </p>
+  const navbarStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '0 2rem',
+    backgroundColor: '#fff', // adjust color
+    height: '80px', // increased height
+  };
 
-      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-        <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
+  const logoStyle = {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    color: '#333', // adjust color
+    padding: '10px 20px',
+    border: '1px solid #333', // border for rectangle
+    borderRadius: '5px', // optional for rounded corners
+  };
+
+  const cartIconStyle = {
+    background: 'none',
+    border: 'none',
+    cursor: 'pointer',
+    position: 'relative',
+  };
+
+  const cartQtyStyle = {
+    position: 'absolute',
+    top: '-10px',
+    right: '-10px',
+    backgroundColor: 'red', // adjust color
+    color: 'white',
+    borderRadius: '50%',
+    padding: '2px 6px',
+    fontSize: '0.8rem',
+  };
+
+  return (
+    <div style={navbarStyle}>
+      <Link href="/">
+        <a style={logoStyle}>BizDelicious</a>
+      </Link>
+
+      <button style={cartIconStyle} onClick={() => setShowCart(true)} aria-label="View Cart">
+        <AiOutlineShopping size={24} />
+        <span style={cartQtyStyle}>{totalQuantities}</span>
       </button>
 
       {showCart && <Cart />}
@@ -24,4 +59,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
